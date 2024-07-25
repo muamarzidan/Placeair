9ceb880d414a78dfc762285967a1b9e8adbdd50e
muamarzidan <codeofomiru11@gmail.com>
2024-07-26 06:06:32 +0700

    feat : setup new

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MainPage from './App.jsx';
+import NotFoundPage from './pages/notfound.jsx';
+import './index.css';
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <MainPage />
+    ),
+  },
+  {
+    path: "*",
+    element: (
+      <NotFoundPage />
+    )
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          '2xl': '5rem',
+        },
+      },
+      colors:{
+        primary: '#181B68',
+      },
+      fontSize: {
+        xs: '10px',
+        sm: '12px',
+        base: '14px',
+        lg: '16px',
+        xl: '18px',
+        '2xl': '20px',
+      },
+    },
   },
   plugins: [],
-}
+};

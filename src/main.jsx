d1aba30b9e088438e085dcfa4f95087434dcf48c
muamarzidan <codeofomiru11@gmail.com>
2024-07-30 03:52:04 +0700

    setup new routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import MainPage from './App.jsx';
+import ExplorePage from './pages/explore/index.jsx';
+import BlogPage from './pages/blog/index.jsx';
+import AboutPage from './pages/about.jsx';
+import ContactPage from './pages/contact.jsx';
 import NotFoundPage from './pages/notfound.jsx';
 import './index.css';
 
@@ -13,6 +17,30 @@ const routes = createBrowserRouter([
     element: (
       <MainPage />
     ),
+  },
+  {
+    path: "/explore",
+    element: (
+      <ExplorePage />
+    ),
+  },
+  {
+    path: "/blog",
+    element: (
+      <BlogPage />
+    ),
+  },
+  {
+    path : "/about",
+    element: (
+      <AboutPage />
+    )
+  },
+  {
+    path: "/contact",
+    element: (
+      <ContactPage />
+    )
   },
   {
     path: "*",

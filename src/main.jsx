b6ac9079152712bee6f3bfb196ad0f55e54bb273
muamarzidan <codeofomiru11@gmail.com>
2024-08-12 20:05:16 +0700

    feat : setup explore destination

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from './App.jsx';
 import ExplorePage from './pages/explore/index.jsx';
 import ExploreDetailProvince from './pages/explore/[province]/index.jsx';
+import ExploreDestinationPage from './pages/explore/destination/index.jsx';
 import BlogPage from './pages/blog/index.jsx';
 import AboutPage from './pages/about.jsx';
 import ContactPage from './pages/contact.jsx';
@@ -29,6 +30,12 @@ const routes = createBrowserRouter([
     path: "/explore/:province",
     element: (
       <ExploreDetailProvince />
+    ),
+  },
+  {
+    path: "/explore-destination",
+    element: (
+      <ExploreDestinationPage />
     ),
   },
   {
--- src/pages/explore/destination/index.jsx
+++ src/pages/explore/destination/index.jsx
@@ -1,29 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Icon } from "@iconify-icon/react";
 
 import Navbar from "../../../components/navbar";
 import Footer from "../../../components/footer";
 import Search from "../../../components/Search";
 import useDebounceSearch from "../../../hooks/debounce";
-import Destination from "../../../api/destionation";
 import formatPrice from "../../../utils/rupiahFormatter";
+import dataDestination from "../../../api/destionation";
+import "../../../assets/css/pages/explore.css";
 
 
-const categories = ["Semua", "Alam", "Sejarah", "Kesenian"];
-
-export default function ExplorePage() {
+export default function ExploreDestinationPage() {
     const [resizeIconStar, setResizeIconStar] = useState("16");
     const [resizeIconLoc, setResizeIconLoc] = useState("24");
-    const [categoryType, setCategoryType] = useState("Semua");
     const [searchDestination, setSearchDestination] = useState("");
-    const debouncedSearchDestination = useDebounceSearch(searchDestination, 300);
+    const debouncedSearch = useDebounceSearch(searchDestination, 500);
+
+    // handle scroll to top page was loaded ( hardcoded :) )
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
     
     // handler resize icon
     const handleResizeIcon = () => {
-        if (window.innerWidth < 480) {
+        if (window.innerWidth < 362) {
+            setResizeIconStar("10");
+            setResizeIconLoc("12");
+        } else if (window.innerWidth < 480) {
             setResizeIconStar("12");
             setResizeIconLoc("14");
         } else if (window.innerWidth < 768) {
+            setResizeIconStar("14");
+            setResizeIconLoc("18");
+        } else if (window.innerWidth < 1280) {
             setResizeIconStar("14");
             setResizeIconLoc("18");
         } else {
@@ -31,7 +41,7 @@ export default function ExplorePage() {
             setResizeIconLoc("24");
         }
     };
-    
+
     useEffect(() => {
         handleResizeIcon();
         // important to add event listener on window when resize the width then resize the size of icon automatically
@@ -42,52 +52,31 @@ export default function ExplorePage() {
         };
     }, []);
 
-    // handler select & search destination
-    const handleCategoryClick = (category) => {
-        setCategoryType(category);
-    };
-    
+    // handler search
+
     const handleSearchDestination = (e) => {
         setSearchDestination(e.target.value);
     };
 
-    const filterMostViewDestination = Destination.filter((data) => {
-        const categoryFilter = categoryType === "Semua" || data.category.toLowerCase() === categoryType.toLowerCase();
-        const searchFilterByCategory = data.name.toLowerCase().includes(debouncedSearchDestination.toLowerCase());
-        return categoryFilter && searchFilterByCategory;
-    }).sort((a, b) => b.viewCount - a.viewCount);
+    const filterDestinationByProvince = dataDestination
+        .filter(destination => destination.name.toLowerCase().includes(debouncedSearch.toLowerCase()));
 
     return (
         <>
             <Navbar />
             <main className="w-full h-auto bg-white">
-                <section id="jelajah" className="w-full h-auto pt-40 pb-20">
-                    <div className="container flex flex-col items-center w-full h-auto gap-1 item">
-                        <h4 className="font-semibold text-[24px] text-secondary text-center">
-                            JELAJAHI TEMPAT IMPIAN ANDA
-                        </h4>
-                        <h3 className="text-6xl font-semibold text-center text-thridly">
-                            Temukan Destinasi Sesuai Keinginan Anda
-                        </h3>
-                        {/* Select category area */}
-                        <ul className="flex w-auto h-auto gap-3 pt-4 text-white">
-                            {categories.map((category) => (
-                                <li
-                                    key={category}
-                                    className={`px-5 py-2 font-semibold rounded-full cursor-pointer ${categoryType === category
-                                        ? "bg-primary text-white"
-                                        : "bg-[#F4F4F4] text-fourly"
-                                        }`}
-                                    onClick={() => handleCategoryClick(category)}
-                                >
-                                    {category}
-                                </li>
-                            ))}
-                        </ul>
-                        {/* Search destination area */}
-                        <div className="flex justify-center w-full h-auto pt-8">
+                <section
+                    id="hero"
+                    className="relative flex flex-col items-center justify-center h-[45vh] sm:h-[80vh] !mt-[75px] md:!mt-3 m-3 bg-center bg-cover rounded-xl"
+                >
+                    <div className="absolute inset-0 bg-black rounded-xl opacity-35"></div>
+                    <div className="relative w-full px-0 lg:px-[70px] xl:px-0">
+                        <h1 id="explore-province-title" className="relative text-4xl sm:text-5xl font-semibold text-center text-white md:text-6xl lg:text-8xl px-[100px] sm:px-[130px] md:px-[100px] lg:px-[66px] xl:px-[140px] 2xl:px-[270px]">
+                            Jelajahi Destinasi Impian di
+                        </h1>
+                        <div className="flex flex-col items-center justify-center h-auto pt-5 md:pt-8">
                             <Search
-                                className="w-full h-auto pr-2 text-3xl text-[#6F706F] placeholder-[#6F706F] outline-none px-9 py-5 peer bg-[#f4f4f4] rounded-full"
+                                className="w-[85%] md:w-[75%] lg:w-[95%] xl:w-[75%] sm:text-lg md:text-xl xl:text-3xl text-[#ffffff] placeholder-[#ffffff]"
                                 type="text"
                                 id="search"
                                 placeholder="Cari destinasi..."
@@ -95,66 +84,69 @@ export default function ExplorePage() {
                                 onChange={handleSearchDestination}
                             />
                         </div>
-                        {/* Popular destination area */}
-                        <div className="flex flex-wrap items-center justify-between w-full h-auto gap-5 pt-5">
-                            {filterMostViewDestination.length > 0 ? (
-                                filterMostViewDestination.map((data, index) => (
+                    </div>
+                </section>
+                <section id="kategori" className="w-full h-auto py-10">
+                    <div className="container flex flex-col w-full h-auto">
+                        <div className="flex flex-wrap items-center justify-between w-full h-auto gap-5 pt-5 explore-container-card">
+                            {filterDestinationByProvince.length > 0 ? (
+                                filterDestinationByProvince.map((data, index) => (
                                     <div
                                         key={index}
-                                        className="w-[250px] h-[306px] sm:w-[300px] sm:h-[356px] md:w-[350px] md:h-[406px] lg:w-full lg:h-[456px] max-w-[400px] max-h-[456px] flex flex-col justify-between p-3 rounded-3xl border-[1px] border-gray-300"
+                                        className="explore-card-des w-[500px] h-[500px] max-w-[279px] max-h-[320px] sm:max-w-[290px] sm:max-h-[320px] md:max-w-[315px] md:max-h-[350px] xl:max-w-[400px] xl:max-h-[430px] 2xl:max-w-[430px] 2xl:max-h-[460px] flex flex-col justify-between p-2 sm:p-3 rounded-[11px] sm:rounded-3xl border-[1px] border-gray-300"
                                     >
+                                        {/* Image card area */}
                                         <div
-                                            className="w-[227px] h-[180px] sm:w-[277px] sm:h-[220px] md:w-[327px] md:h-[230px] lg:w-full lg:h-full !max-w-[377px] !max-h-[280px] p-3 rounded-xl"
+                                            className="explore-des-cardImage w-full h-[67%] sm:w-full sm:h-[67%] md:h-[68%] lg:h-[67%] xl:h-full !max-w-[404px] !max-h-[295px] p-2 sm:p-3 rounded-[8px] sm:rounded-xl"
                                             style={{
                                                 backgroundImage: `url(${data.thumbnail})`,
                                                 backgroundSize: "cover",
                                                 backgroundPosition: "center",
                                             }}
                                         >
-                                            <div className="flex items-center justify-evenly w-[67px] py-2 px-2 rounded-full bg-[#ffffff2b]">
-                                                <Icon
-                                                    icon="mingcute:star-fill"
-                                                    style={{ color: "#ff9b48" }}
-                                                    width={resizeIconStar}
-                                                    height={resizeIconStar}
-                                                />
-                                                <span className="text-xs text-white sm:text-sm">
-                                                    {data.rating}
-                                                </span>
-                                            </div>
+                                        <div className="explore-prov-rate flex items-center justify-evenly w-[57px] sm:w-[67px] py-[6px] sm:py-2 px-1 sm:px-2 rounded-full bg-[#ffffff42]">
+                                            <Icon
+                                                icon="mingcute:star-fill"
+                                                style={{ color: "#ff9b48" }}
+                                                width={resizeIconStar}
+                                                height={resizeIconStar}
+                                            />
+                                            <span className="text-xs text-white sm:text-sm">
+                                                {data.rating}
+                                            </span>
                                         </div>
-                                        <span className="w-fit text-[#171717] font-bold sm:text-xl md:text-3xl">
+                                        </div>
+                                        <span className="card-des-title w-fit text-[#171717] font-bold text-md sm:text-xl lg:text-2xl xl:text-3xl">
                                             {data.name}
                                         </span>
-                                        <div className="flex items-center gap-1 sm:gap-2 w-fit">
+                                        {/* Icon and link location area */}
+                                        <Link to={data.locationLink} target="_blank" className="flex items-center gap-1 sm:gap-2 w-fit">
                                             <Icon
                                                 icon="fluent:location-16-filled"
                                                 style={{ color: "#4c82fe" }}
                                                 width={resizeIconLoc}
                                                 height={resizeIconLoc}
                                             />
-                                            <span className="text-sm sm:text-md md:text-xl">
-                                                {data.location}
-                                                <a href={data.locationLink}></a>
-                                            </span>
-                                        </div>
+                                            <span className="text-sm card-loc-des sm:text-md xl:text-xl">{data.location}</span>
+                                        </Link>
+                                        {/* Price and button area */}
                                         <div className="flex items-center justify-between w-full">
-                                            <span className="font-bold text-md sm:text-[16px] md:text-[26px] text-[#171717]">
+                                            <span className="card-des-price font-bold text-md sm:text-2xl md:text-[20px] lg:text-[22px] xl:text-[26px] text-[#171717]">
                                                 {formatPrice(data.price)}
                                             </span>
-                                            <button className="px-3 py-2 text-sm font-bold text-white rounded-full sm:px-5 md:py-3 sm:text-md md:text-lg bg-primary">
-                                                Selengkapnya
+                                            <button className="card-des-button px-5 py-2 sm:py-2 text-sm font-normal text-white rounded-full md:font-semibold sm:px-8 md:px-8 md:py-[10px] sm:text-md md:text-lg bg-primary">
+                                                Lihat
                                             </button>
                                         </div>
                                     </div>
-                                ))) : (
-                                    <div className="flex items-center justify-center w-full h-auto p-5">
-                                        <h4 className="font-semibold text-[24px] text-secondary text-center">
-                                            Destinasi tidak ditemukan
-                                        </h4>
-                                    </div>
-                                )
-                            }
+                                ))
+                            ) : (
+                                <div className="flex items-center justify-center w-full h-auto p-5">
+                                    <h4 className="font-semibold text-md sm:text-2xl md:text-[24px] text-secondary text-center">
+                                        Destinasi tidak ditemukan di provinsi
+                                    </h4>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </section>
